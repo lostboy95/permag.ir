429dc854f158423d35ae5c865f444323f54d7b42
lostboy95 <Hamedasadzadeh9@gmail.com>
2017-04-21 20:41:55 +0430

    Create about.lua

---
+++ plugins/about.lua
@@ -0,0 +1,29 @@
+do
+
+function run(msg, matches)
+  return [[
+🌐IRAN Mega Bot 
+💠ver: 2.1
+💠2018
+💠Admin:
+ @HUA2018 - @MEGAPMROBOT
+
+
+🔅تیم برنانه نویسی  مگا بوت 🔅
+🔅ID ch: @IRmega🔅
+  ]]
+
+  end
+return {
+  description = "shows more", 
+  usage = "more : return sudousers",
+  patterns = {
+    "^([Mm][Oo][Rr][Ee])$",   
+    "^[!$#/](more)$",
+},
+patterns_fa = {
+"^(درباره)$", 
+  },
+  run = run
+}
+end
